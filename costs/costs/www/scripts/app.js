6d069e6368cce4f5dae13fb1a49312653cc38a19
eugene afanasovich <eugeneafanasovich@gmail.com>
2016-02-11 16:33:03 +0300

    хз

--- costs/costs/www/scripts/app.js
+++ costs/costs/www/scripts/app.js
@@ -3,7 +3,7 @@ mainApp.config(function ($routeProvider) {
     $routeProvider
     .when('/', {
         templateUrl: 'html/main_page.html',
-        controller: 'mainController'
+        controller: 'startController'
     })
     .when('/main_page', {
         templateUrl: 'html/main_page.html',
@@ -12,5 +12,21 @@ mainApp.config(function ($routeProvider) {
     .when('/add_event_page', {
         templateUrl: 'html/add_event_page.html',
         controller: 'addEventController'
+    })
+    .when('/event_page', {
+        templateUrl: 'html/event_page.html',
+        controller: 'eventController'
+    })
+    .when('/add_member_page', {
+        templateUrl: 'html/add_member_page.html',
+        controller: 'addMemberController'
+    })
+    .when('/member_page', {
+        templateUrl: 'html/member_page.html',
+        controller: 'memberController'
+    })
+    .when('/add_pay_page', {
+        templateUrl: 'html/add_pay_page.html',
+        controller: 'addPayController'
     });
 });
--- costs/costs/www/scripts/controllers.js
+++ costs/costs/www/scripts/controllers.js
@@ -1,56 +1,396 @@
-﻿mainApp.controller('mainController', function ($scope, $http, $location)
-{
-    $http({
-        method: 'GET',
-        url: 'data/events.json'
-    }).then(function successCallback(response) {
-        $scope.events = response.data.events;
-    },
-    function errorCallback(response) {
-        var error = response;
-    });
-});
+﻿function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
 
-mainApp.controller('addEventController', function ($scope, $http, $location)
+mainApp.controller('mainController', function ($scope, $http, $location)
 {
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-    function onSuccess(fileSystem)
-    {
-        var a = fileSystem.name;
-        var b = fileSystem.root.name;
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
     }
 
-    function gotFS(fileSystem)
-    {
-        var a = fileSystem.name;
-        var b = fileSystem.root.name;
-
-        fileSystem.root.getFile("readme.txt", { create: true, exclusive: false }, gotFileEntry, fail);
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("events.json", null, gotFileEntry, fail);
     }
 
     function gotFileEntry(fileEntry) {
-        fileEntry.createWriter(gotFileWriter, fail);
+        fileEntry.file(gotFile, fail);
     }
 
-    function gotFileWriter(writer) {
-        writer.onwriteend = function (evt) {
-            console.log("contents of file now 'some sample text'");
-            writer.truncate(11);
-            writer.onwriteend = function (evt) {
-                console.log("contents of file now 'some sample'");
-                writer.seek(4);
-                writer.write(" different text");
-                writer.onwriteend = function (evt) {
-                    console.log("contents of file now 'some different text'");
-                }
-            };
+    function gotFile(file) {
+        readAsText(file);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (evt)
+        {
+            var t = JSON.parse(evt.target.result);
+            $scope.$apply(function () {
+                $scope.events = t;
+            });
+            $scope.events = t;
         };
-        writer.write("some sample text");
+        reader.readAsText(file);
     }
 
     function fail(error) {
         console.log(error.code);
     }
 
+    $scope.viewEvent = function (id) {
+        document.cookie = "event_id="+id;
+        window.location("#event_page");
+    };
+});
+
+mainApp.controller('addEventController', function ($scope, $http, $location)
+{
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("events.json", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file) {
+        readAsText(file);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (evt) {
+            var t = JSON.parse(evt.target.result);
+            $scope.events = t;
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
+
+    $scope.addEvent = function () {
+
+        document.addEventListener("deviceready", onDeviceReady, false);
+
+        function onDeviceReady() {
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+        }
+
+        function gotFS(fileSystem) {
+            fileSystem.root.getFile("events.json", { create: true, exclusive: false }, gotFileEntry, fail);
+        }
+
+        function gotFileEntry(fileEntry) {
+            fileEntry.createWriter(gotFileWriter, fail);
+        }
+
+        function gotFileWriter(writer)
+        {
+            writer.onwriteend = function (evt)
+            {
+                window.location = "#";
+            };
+
+            var id = 0;
+            var myArray = $scope.events;
+            myArray.map(function (ele)
+            {
+                if (ele.id > id)
+                {
+                    id = ele.id;
+                }
+            });
+
+            var name = document.getElementById("name").value;
+            myArray.push({ "id": id + 1, "name": name });
+            var data = JSON.stringify(myArray);
+            writer.write(data);
+        }
+
+        function fail(error) {
+            console.log(error.code);
+        }
+
+    };
+});
+
+mainApp.controller('startController', function ($scope, $http, $location)
+{
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("events.json", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file) {
+        window.location = "#main_page";
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
+});
+
+mainApp.controller('eventController', function ($scope, $http, $location)
+{
+    var event_id = getCookie("event_id");
+    $scope.event_id = event_id;
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("members.json", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file) {
+        readAsText(file);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (evt) {
+            var t = JSON.parse(evt.target.result);
+            $scope.$apply(function () {
+                $scope.members = t;
+            });
+            $scope.members = t;
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
+
+    $scope.viewMember = function (id) {
+        document.cookie = "member_id=" + id;
+        window.location("#member_page");
+    };
+});
+
+mainApp.controller('addMemberController', function ($scope, $http, $location)
+{
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("members.json", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file) {
+        readAsText(file);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (evt) {
+            var t = JSON.parse(evt.target.result);
+            $scope.events = t;
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
+
+    $scope.addMember = function () {
+
+        document.addEventListener("deviceready", onDeviceReady, false);
+
+        function onDeviceReady() {
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+        }
+
+        function gotFS(fileSystem) {
+            fileSystem.root.getFile("members.json", { create: true, exclusive: false }, gotFileEntry, fail);
+        }
+
+        function gotFileEntry(fileEntry) {
+            fileEntry.createWriter(gotFileWriter, fail);
+        }
+
+        function gotFileWriter(writer) {
+            writer.onwriteend = function (evt) {
+                window.location = "#event_page";
+            };
+
+            var id = 0;
+            var myArray = $scope.events;
+            myArray.map(function (ele) {
+                if (ele.id > id) {
+                    id = ele.id;
+                }
+            });
+
+            var event_id = getCookie("event_id");
+
+            var name = document.getElementById("name").value;
+            myArray.push({ "id": id + 1,"event_id": event_id, "name": name });
+            var data = JSON.stringify(myArray);
+            writer.write(data);
+        }
+
+        function fail(error) {
+            console.log(error.code);
+        }
+
+    };
+});
+
+mainApp.controller('memberController', function ($scope, $http, $location) {
+
+    var member_id = getCookie("member_id");
+    $scope.member_id = member_id;
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("pays.json", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file) {
+        readAsText(file);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (evt) {
+            var t = JSON.parse(evt.target.result);
+            $scope.$apply(function () {
+                $scope.pays = t;
+            });
+            $scope.pays = t;
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
+});
+
+mainApp.controller('addPayController', function ($scope, $http, $location) {
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("pays.json", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file) {
+        readAsText(file);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (evt) {
+            var t = JSON.parse(evt.target.result);
+            $scope.events = t;
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(error) {
+        console.log(error.code);
+    }
+
+    $scope.addPay = function () {
+
+        document.addEventListener("deviceready", onDeviceReady, false);
+
+        function onDeviceReady() {
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+        }
+
+        function gotFS(fileSystem) {
+            fileSystem.root.getFile("pays.json", { create: true, exclusive: false }, gotFileEntry, fail);
+        }
+
+        function gotFileEntry(fileEntry) {
+            fileEntry.createWriter(gotFileWriter, fail);
+        }
+
+        function gotFileWriter(writer) {
+            writer.onwriteend = function (evt) {
+                window.location = "#member_page";
+            };
+
+            var id = 0;
+            var myArray = $scope.events;
+            myArray.map(function (ele) {
+                if (ele.id > id) {
+                    id = ele.id;
+                }
+            });
+
+            var member_id = getCookie("member_id");
+
+            var pay = document.getElementById("pay").value;
+            myArray.push({ "id": id + 1, "member_id": member_id, "pay": pay });
+            var data = JSON.stringify(myArray);
+            writer.write(data);
+        }
+
+        function fail(error) {
+            console.log(error.code);
+        }
+
+    };
 });
